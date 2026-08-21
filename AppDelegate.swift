8bafcb60519ed894e7eb27b9cd679ebe303999cb
leos978 <leos978@gmail.com>
2026-08-21 17:05:30 +0200

    Fallback AppDelegate UIWindow

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -1,14 +1,24 @@
 import UIKit
 import FirebaseCore
 import GoogleSignIn
+import SwiftUI
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
 
     func application(_ application: UIApplication, 
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Inizializza Firebase
         FirebaseApp.configure()
+        
+        // Fallback per iOS che non carica SceneDelegate
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = MainViewController()
+        window.makeKeyAndVisible()
+        self.window = window
+        
         return true
     }
 
