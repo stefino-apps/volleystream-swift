4a97acdec1ef4a5fae084af1fb43196e7188fc60
leos978 <leos978@gmail.com>
2026-08-29 19:32:18 +0200

    Fix AppDelegate SceneDelegate and Info.plist bundle ID

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -33,6 +33,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, 
                      configurationForConnecting connectingSceneSession: UISceneSession, 
                      options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+        let config = UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+        config.delegateClass = SceneDelegate.self
+        return config
     }
 }
